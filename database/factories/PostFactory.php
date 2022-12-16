175c30da37276805b4ac3b748c05d50c4c369bc9
hxbc01 <muhammadarif4325@gmail.com>
2022-12-16 20:50:41 +0700

    push to github

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => rand(1,5),
+            'title'=>$this->faker->name(),
+            'comment'=>$this->faker->realText(180),
+
+        ];
+    }
+
+    
+}
